df558c57281452ebf1f618bef6f5313fb8213dba
daverydan <danny.w.avery@gmail.com>
2024-05-14 21:01:32 -0400

    Remove unneeded code in AppMenu

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -22,15 +22,4 @@
 
 <script setup>
 const isOpen = ref(false);
-
-const ui = {
-	wrapper: 'border-s border-gray-200 dark:border-gray-800 space-y-2',
-	base: 'group block border-s -ms-px leading-6 before:hidden',
-	padding: 'p-0 ps-4',
-	rounded: '',
-	font: '',
-	ring: '',
-	active: 'text-primary-500 dark:text-primary-400 border-current font-semibold',
-	inactive: 'border-transparent hover:border-gray-400 dark:hover:border-gray-500 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300'
-};
 </script>
